67a5f10a6ada7dbbd275365a4645e04d1a20912d
MelodyBaird <melodyabaird@gmail.com>
2025-09-30 14:02:01 +1300

    updated vid cap

--- System Logic/Drafts/videoCapture.py
+++ System Logic/Drafts/videoCapture.py
@@ -5,6 +5,24 @@ HOST = "0.0.0.0"
 PORT = 5000
 buffer = ""
 
+indexRanging1 = [(175, 225), (225, 275), (275, 325), (325, 375)]
+indexRanging2 = [(325, 375), (275, 325), (225, 275), (175, 225)]
+
+colourCheck = 1
+colourTarget = [0, 0, 0, 0]
+coloursFound = 0
+colours = []
+colourPos = []
+target = True
+targetPos = []
+run = True
+sender = ''
+receiver = ''
+colourScan = True
+targetNumbers = []
+firstTarget = -1
+secondTarget = -1
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
@@ -45,6 +63,9 @@ url = "http://192.168.137.136:81/stream"
 cap = cv2.VideoCapture(url)  # Open the ESP32-CAM stream
 
 while True:
+    rightSensor = distance[0]
+    frontSensor = distance[1]
+    backAverage = (distance[3] + distance[4]) / 2
     success, bgr = cap.read()
     width = int(cap.get(3))  # Get the width of the frame
     height = int(cap.get(4))  # Get the height of the frame
@@ -58,12 +79,6 @@ while True:
     red1 = cv2.inRange(hsv, lowerRed, upperRed)
     red2 = cv2.inRange(hsv, lowerRed2, upperRed2)
     red = red1 + red2  # Combine the two red masks
-
-    # Create masks for each color
-    blueMask = cv2.bitwise_and(bgr, bgr, mask=blue)
-    greenMask = cv2.bitwise_and(bgr, bgr, mask=green)
-    yellowMask = cv2.bitwise_and(bgr, bgr, mask=yellow)
-    redMask = cv2.bitwise_and(bgr, bgr, mask=red)
     
     # Find contours for each color mask
     blueContours, blueHierarchy=cv2.findContours(blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -77,25 +92,80 @@ while True:
             if cv2.contourArea(blueContour) > 500:
                 x, y, w, h = cv2.boundingRect(blueContour)
                 cv2.rectangle(bgr, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                colour = [3, x, y, w, h]
                 
     if len(greenContours) != 0:
         for greenContour in greenContours:
             if cv2.contourArea(greenContour) > 500:
                 x, y, w, h = cv2.boundingRect(greenContour)
                 cv2.rectangle(bgr, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if colour == []:
+                    colour = [2, x, y, w, h]
+                elif (w * h) > (colour[3] * colour[4]):
+                    colour = [2, x, y, w, h]
                 
     if len(yellowContours) != 0:
         for yellowContour in yellowContours:
             if cv2.contourArea(yellowContour) > 500:
                 x, y, w, h = cv2.boundingRect(yellowContour)
                 cv2.rectangle(bgr, (x, y), (x + w, y + h), (0, 255, 255), 2)
+                if colour == []:
+                    colour = [1, x, y, w, h]
+                elif (w * h) > (colour[3] * colour[4]):
+                    colour = [1, x, y, w, h]
                 
     if len(redContours) != 0:
         for redContour in redContours:
             if cv2.contourArea(redContour) > 500:
                 x, y, w, h = cv2.boundingRect(redContour)
                 cv2.rectangle(bgr, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                if colour == []:
+                    colour = [0, x, y, w, h]
+                elif (w * h) > (colour[3] * colour[4]):
+                    colour = [0, x, y, w, h]
+
+    if rightSensor > 100:
+        target = False
+
+    if not(colour == []): 
+        if target: # looking for target colours
+            colourTarget[colour[0]] = 1
+        else:
+            if colourTarget[colour[0]] and rightSensor > 100:
+                print('target found')
+                if frontSensor > backAverage:
+                    for i in range(0, len(indexRanging1)):
+                        if backAverage in range(indexRanging1[i]):
+                            if (i) not in targetNumbers:
+                                targetNumbers.append(i)
+                else:
+                    for i in range(0, len(indexRanging2)):
+                        if frontSensor in range(indexRanging1[i]):
+                            if (i + 4) not in targetNumbers:
+                                targetNumbers.append(i + 4)
+                if targetNumbers == []:
+                    print('out of ranges')
+                else:
+                    print(targetNumbers)
+                    
+    if len(targetNumbers) >= 1:
+        firstTarget = targetNumbers[0]
+        msg = f"setFirstTarget:{firstTarget}\n"
+        conn.sendall(msg.encode())
+
+    if len(targetNumbers) == 2:
+        secondTarget = targetNumbers[1]
+        msg = f"setSecondTarget:{secondTarget}\n"
+        conn.sendall(msg.encode())
+        
                 
+
+
+                
+
+
+
+
     # Display the processed images
     cv2.imshow('Original', bgr)
 
